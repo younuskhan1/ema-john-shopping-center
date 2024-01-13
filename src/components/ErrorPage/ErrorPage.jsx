565cf54dad6d9c2d619a92ab82cb8ee3505549c5
younuskhan1 <md.younuskhan412@gmail.com>
2024-01-13 19:18:26 +0600

    error message correction is done

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -1,8 +1,15 @@
-
+import { NavLink } from "react-router-dom";
+import "./ErrorPage.css";
+import image from "../../images/no-data-found.jpg";
 const ErrorPage = () => {
     return (
-        <div>
-            <h1>There is nothing what you are finding out</h1>
+        <div className="error-page">
+            <div> 
+            <div className="image-div"><img className="error-image" src={image} alt="" /></div>
+            <p className="error-heading">Oops !!!</p>
+            <p className="error-message">Sorry, The Data is not Available</p>
+            <p className="error-nav"><NavLink to = "/">GO TO HOME</NavLink></p>
+            </div>
         </div>
     );
 };
