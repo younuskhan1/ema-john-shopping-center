d591d70b3498a2e3332c24f0876ea43a4f7c77b5
younuskhan1 <md.younuskhan412@gmail.com>
2024-01-15 23:46:04 +0600

    card style is like rokomari.com is done

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -19,8 +19,10 @@ const Product = ({product, ProductCardButtonHandler}) => {
                     <p className='rating-parent'> Reviews  <span><Rating emptySymbol={<i className="fa-regular fa-star"></i>}
                     fullSymbol={<i className="fa-solid fa-star"></i>} initialRating={ratings} readonly/></span></p>
                 </div>
-                <div className='button-container'><button className='product-add-button' onClick={()=>ProductCardButtonHandler(product)}>Add To Cart<span className='cart-icon'><i className="fa-solid fa-cart-plus"></i></span></button></div>
-                <div className='view-details-button-parent'><button className='view-details-button'>View Details</button></div>        
+                <div className='card-two-buttons-container'>
+                    <div className='add-button-container'><button className='product-add-button' onClick={()=>ProductCardButtonHandler(product)}>Add To Cart<span className='cart-icon'><i className="fa-solid fa-cart-plus"></i></span></button></div>
+                    <div className='view-details-button-parent'><button className='view-details-button'>View Details</button></div>        
+                </div>
             </div>
         </div>
     );
