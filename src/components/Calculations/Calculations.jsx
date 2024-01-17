88d8ec2d5829fc4c18631a14618e596e886d9615
younuskhan1 <md.younuskhan412@gmail.com>
2024-01-17 16:23:05 +0600

    orders review's calculations work is left now

--- src/components/Calculations/Calculations.jsx
+++ src/components/Calculations/Calculations.jsx
@@ -3,8 +3,16 @@ import "./Calculations.css";
 const Calculations = () => {
     return (
         <div className="calculations-parent">
-            <div>
+            <div className="calculations-sub-parent">
                 <h2 className="calculations-summary">Orders Summary</h2>
+                <div className="calculated-information">
+                  <p>Selected Items : </p>
+                  <p>Total Price : $</p>
+                  <p>Total Shipping Charge : $</p>
+                  <p>Total Payable Tax : $</p>
+                  <p><span className="grand-total">Grand Total : $</span></p>
+                </div>
+                <div><button className='orders-review-clear-cart-button'>Clear Cart <span className='orders-review-cart-button-icon'><i className="fa-solid fa-trash-can"></i></span></button></div>
             </div>
         </div>
     );
